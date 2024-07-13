4c86dbc5548417c36e5b54550e3500059f86be8f
Gabriel Setznagl <gabrielembraer3@gmail.com>
2024-07-12 21:19:40 -0300

    .
    .

--- src/main/java/org/example/hibernate/JPA/Main.java
+++ src/main/java/org/example/hibernate/JPA/Main.java
@@ -1,4 +1,6 @@
-package org.example.hibernate;
+package org.example.hibernate.JPA;
+
+import org.junit.jupiter.api.Test;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
